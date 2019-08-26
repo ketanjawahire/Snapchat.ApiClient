7b089dcd456515d12d80d0665009209bfb900bbf
Ketan <ketanjawahire2012@gmail.com>
2019-08-26 19:43:34 +0530

    committing program.cs file for sample application

---
+++ src/Snapchat.ApiClient/SampleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using Snapchat.ApiClient;
+using System;
+
+namespace SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiServices = new SnapchatServices("client_Id", "client_secret", "refresh_token");
+
+            var organizationService = apiServices.GetService<IOrganizationService>();
+            var accountService = apiServices.GetService<IAdAccountService>();
+            var campaignService = apiServices.GetService<ICampaignService>();
+        }
+    }
+}
