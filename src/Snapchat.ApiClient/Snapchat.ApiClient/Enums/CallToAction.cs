89d75be2ac087067c262f84b0be9e22ab8b41001
Ketan <ketanjawahire2012@gmail.com>
2019-09-03 18:34:00 +0530

    added few more CTAs

--- src/Snapchat.ApiClient/Snapchat.ApiClient/Enums/CallToAction.cs
+++ src/Snapchat.ApiClient/Snapchat.ApiClient/Enums/CallToAction.cs
@@ -6,7 +6,26 @@
         BLANK,
         INSTALL_NOW,
         WATCH,
-        VIEW_MORE
+        VIEW_MORE,
+        WATCH_EPISODE,
+        DOWNLOAD,
+        PLAY,
+        SHOP_NOW,
+        SIGN_UP,
+        USE_APP,
+        APPLY_NOW,
+        BOOK_NOW,
+        BUY_TICKETS,
+        GET_NOW,
+        LISTEN,
+        MORE,
+        ORDER_NOW,
+        READ,
+        SHOW,
+        SHOWTIMES,
+        VIEW,
+        OPEN_APP,
+        TRY,
 #pragma warning restore CA1707 // Identifiers should not contain underscores
     }
 }
