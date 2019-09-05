81c521c059e2df795367bda8c9dcd4d4b0db6870
Ketan <ketanjawahire2012@gmail.com>
2019-09-06 00:02:19 +0530

    updated sample application code

--- src/Snapchat.ApiClient/SampleApplication/Program.cs
+++ src/Snapchat.ApiClient/SampleApplication/Program.cs
@@ -1,5 +1,5 @@
-﻿using Snapchat.ApiClient;
-using System;
+﻿using Snapchat.ApiClient.Services;
+using Snapchat.ApiClient.Services.Interfaces;
 
 namespace SampleApplication
 {
@@ -7,11 +7,32 @@ namespace SampleApplication
     {
         static void Main(string[] args)
         {
-            var apiServices = new SnapchatServices("client_Id", "client_secret", "refresh_token");
+            //Initialize snapchat service with client id, client secret & refresh token. 
+            var snapchatServices = new SnapchatServices("clientId", "clientSecret", "refreshToken");
 
-            var organizationService = apiServices.GetService<IOrganizationService>();
-            var accountService = apiServices.GetService<IAdAccountService>();
-            var campaignService = apiServices.GetService<ICampaignService>();
+            //Get services to interact with ad account object
+            var accountService = snapchatServices.GetService<IAdAccountService>();
+            var adAccount = accountService.GetByAccountId("accountId");
+
+            //Get services to interact with ad object
+            var adService = snapchatServices.GetService<IAdService>();
+            var ad = adService.Get("adId");
+
+            //Get services to interact with campaign object
+            var campaignService = snapchatServices.GetService<ICampaignService>();
+            var campaign = campaignService.GetById("campaignId");
+
+            //Get services to interact with ad squad object
+            var adSquadService = snapchatServices.GetService<IAdSquadService>();
+            var adSquad = adSquadService.Get("adSquadId");
+
+            //Get services to interact with creative object
+            var creativeService = snapchatServices.GetService<ICreativeService>();
+            var creative = creativeService.Get("creativeId");
+
+            //Get services to interact with organization object
+            var organizationService = snapchatServices.GetService<IOrganizationService>();
+            var organization = organizationService.GetById("organizationId");
         }
     }
 }
