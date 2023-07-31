fb3b92606e47cc34ef446563b6490d9b31b6b7f9
vishnudevi.t@deltax.com <vishnudevi.t@deltax.com>
2023-07-31 14:40:58 +0530

    changes for adding composite properties refs DX-36877

---
+++ src/Snapchat.ApiClient/Snapchat.ApiClient/Entities/Api/CompositeProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Snapchat.ApiClient.Entities.Api
+{
+    /// <summary>
+    /// Represents Snapchat composite properties.
+    /// </summary>
+    public class CompositeProperties
+    {
+        /// <summary>
+        /// Gets or sets url.
+        /// </summary>
+        [JsonProperty("creative_ids")]
+#pragma warning disable CA2227 // Collection properties should be read only
+        public List<string> CreativeIds { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+    }
+}
